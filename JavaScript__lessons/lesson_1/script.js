49a2d64ce0b5d785103ae80259c8ed75168dc75f
Vasiliy <roycejs@yandex.ru>
2019-03-17 00:11:01 +1000

    performed task 1

--- JavaScript__lessons/lesson_1/script.js
+++ JavaScript__lessons/lesson_1/script.js
@@ -1,1 +1,20 @@
-var firstVariable;
+var firstQuestion = +prompt("Ваш бюджет?", "500 rub");
+var secondQuestion = prompt("Название вашего магазина?", "Aliexpress");
+
+var mainList = {};
+
+mainList = {
+	budget: 0,
+	nameShop: "",
+	shopGoods: [],
+	employers: {},
+	open: ""
+}
+
+mainList.nameShop = secondQuestion;
+mainList.budget = firstQuestion;
+
+
+console.log(mainList.nameShop);
+console.log(mainList.budget);
+console.log( typeof(mainList.shopGoods));
